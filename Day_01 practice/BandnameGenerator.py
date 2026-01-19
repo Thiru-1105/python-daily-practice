4ec5c4dea846468c9a37ebaef43e028c199aea0c
Thiru-1105 <thirumurugans.work@gmail.com>
2026-01-19 20:04:59 +0530

    Day1: Bandname generator project

---
+++ Day_01 practice/BandnameGenerator.py
@@ -0,0 +1,5 @@
+print("Hello Welcome to the Band Name Generator Application !!")
+city=input("Which city did you grew up ?\n")
+pet=input("Name any of your favorite pet ?\n")
+print("Your band can be names as : "+city+" "+pet)
+
